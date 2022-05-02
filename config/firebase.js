41ee7b0a82ac1ef6c603eeda3b316ffced138efd
Ana Isabel Iturbe <anaisabeliturbec@gmail.com>
2022-05-02 11:54:24 -0400

    initial commit

---
+++ config/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB-8Mi0VHXqeJbXeIPIKnevUAID18ooC5s",
+  authDomain: "fbdb-833ab.firebaseapp.com",
+  projectId: "fbdb-833ab",
+  storageBucket: "fbdb-833ab.appspot.com",
+  messagingSenderId: "247869032312",
+  appId: "1:247869032312:web:01573f88eb490e69512a8d"
+};
+
+let Firebase;
+
+if (firebase.apps.length === 0) {
+  Firebase = firebase.initializeApp(firebaseConfig);
+}
+
+export default Firebase;
