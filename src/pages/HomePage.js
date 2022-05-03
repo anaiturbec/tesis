13725f9b312bd4444a4c37121892b31510ea1a07
Ana Isabel Iturbe <anaisabeliturbec@gmail.com>
2022-05-03 15:21:49 -0400

    nav component created

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+export default function HomePage() {
+  return (
+    <div className="w-full h-full overflow-hidden flex flex-col">
+      <NavBar />
+    </div>
+  );
+}
